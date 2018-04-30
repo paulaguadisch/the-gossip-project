2d44f6fcbbc6e2f629371bde2452549a344c5d62
Paul A <paul.aguadisch@gmail.com>
2018-04-30 18:55:47 +0200

    ajout de mes dernières modifs avant les suivantes

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,42 @@
+class GossipsController < ApplicationController
+  def new
+  	@gossip = Gossip.new
+  end
+
+  def create
+  	@gossip = Gossip.create(gossip_params)
+  	redirect_to gossip_path(@gossip.id)
+  end
+
+  def show
+  	@gossip = Gossip.find(params[:id])
+  end
+
+  def edit
+  	@gossip = Gossip.find(params[:id])
+  end
+
+  def update
+  	@gossip = Gossip.find(params[:id])
+  	@gossip.update(gossip_params)
+  	redirect_to gossips_path(@gossip.id)
+  end
+
+  def destroy
+  	@gossip = Gossip.find(params[:id])
+  	@gossip.destroy
+  	redirect_to gossips_path(@gossip.id)
+  end
+
+  def index
+
+  end
+
+
+  private
+
+  def gossip_params
+  	params.require(:gossip).permit(:anonymous_author, :content)
+  end
+
+end
