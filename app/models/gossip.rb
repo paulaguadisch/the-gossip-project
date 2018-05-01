32d4c15088ff0d700f641066b0d65090a2599f47
Paul A <paul.aguadisch@gmail.com>
2018-05-01 21:09:10 +0200

    dernieres modifs

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,7 @@
 class Gossip < ApplicationRecord
+	has_many :comments
+
+
 	validates :anonymous_author, presence: true
 	validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_130949) do
+ActiveRecord::Schema.define(version: 2018_05_01_103848) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "anonymous_comment"
+    t.string "body"
+  end
 
   create_table "gossips", force: :cascade do |t|
     t.string "anonymous_author"
